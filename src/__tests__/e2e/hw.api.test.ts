d25ab1b6f79b4177039a116f3b01d566936c813d
Bucktabu <stanislaworonin@yandex.ru>
2022-10-02 14:09:23 +0300

    fix test authorization

--- src/__tests__/e2e/hw.api.test.ts
+++ src/__tests__/e2e/hw.api.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import {app} from "../../index";
 
 describe('/posts', () => {
-    it('Expected 401', async  () => {
+    it('Expected 401 - unauthorized', async  () => {
         await request(app)
             .post('/blogs')
             .send({
@@ -10,5 +10,16 @@ describe('/posts', () => {
                 "youtubeUrl": "https://www.youtube.com/watch?v=vuzKKCYXISA&t=2677s&ab_channel=IT-KAMASUTRA"
             })
             .expect(401)
+    })
+
+    it('Expected 400 - bad request', async () => {
+        await request(app)
+            .post('/blogs')
+            .send({
+                "name": "",
+                "youtubeUrl": ""
+            })
+            .set({Authorization: 'Basic YWRtaW46cXdlcnR5'})
+            .expect(400)
     })
 })
